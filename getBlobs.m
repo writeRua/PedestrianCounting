bb3f78e7dd1fb47439113020bb12bed5fcebdcdb
Qi <zhouqi2013@gmail.com>
2016-06-12 14:33:49 +0800

    fixed bugs: ground truth counting
    counting the number of pedestrians within a certain blob ====> considering nearest blob for each pedestrian

--- getBlobs.m
+++ getBlobs.m
@@ -1,10 +1,9 @@
-function [blobs_mask, blobFeatures] = getBlobs(original_img,foreground_mask,dmap,roi,fg_info,FeatureName,opt)
+function [blobs_mask, blobs_Features] = getBlobs2(original_img,foreground_mask,dmap,roi,fg_info,FeatureName,opt)
 
 	% find all blobs and get features of each blob
 
 	if nargin < 7
 		opt.min_blob = 10;
-		opt.gt_boundary_count = 1;
 		opt.gt_nodirection_count = 1;
 		opt.gt_only_roi_count = 1;
 	end
@@ -12,9 +11,13 @@ function [blobs_mask, blobFeatures] = getBlobs(original_img,foreground_mask,dmap
 	[size_x, size_y] = size(roi);
 
     blobs_mask = {};  		% mask of each blob in current frame
-    blobFeatures = [];		% features of each blob in current frame
+    blobs_Features = [];	% features of each blob in current frame
+    blobs_dir = [];         % direction of each blob
+    blobs_gt = [];          % ground truth of each blob
+
 
     while find(foreground_mask>0)>0
+        % get blob mask
         init_dot = find(foreground_mask>0);
         init_dot = init_dot(1);
         cur_mask = zeros(size(foreground_mask));
@@ -30,12 +33,13 @@ function [blobs_mask, blobFeatures] = getBlobs(original_img,foreground_mask,dmap
         if length(find(cur_mask)) < opt.min_blob
             continue
         end
+        blobs_mask{length(blobs_mask)+1} = cur_mask;
 
         % get features of current blob
         feat = blob2feature(original_img, cur_mask, dmap, FeatureName);
-        blobs_mask{length(blobs_mask)+1} = cur_mask;
+        blobs_Features = [blobs_Features; feat];
         
-        % direction
+        % get direction of current blob
         switch direction_number
             case 128
                 % r(right) or rs(right slow)
@@ -49,41 +53,39 @@ function [blobs_mask, blobFeatures] = getBlobs(original_img,foreground_mask,dmap
             case 196
                 % lf(left fast)
                 tmp_dir = 4;
+            otherwise
+                error('Unknow direction!');
         end
-
-        % get grount truth number of current blob
-        gtc = 0;
-        for i=1:size(fg_info.loc, 1)
-            y = round(fg_info.loc(i,1));
-            x = round(fg_info.loc(i,2));
-
-            % what if the position of a pedestrian outside of the boundary
-            % (maybe a part of the blob in the ROI)
-            if opt.gt_boundary_count
-                if (x<=0); x = 1; end
-                if (x>size_x); x = size_x; end
-                if (y<=0); y = 1; end
-                if (y>size_y); y = size_y; end
-            elseif x <= 0 || x > size_x || y <=0 || y > size_y
-                continue
-            end
-
-            % static pedestrian
-            if opt.gt_nodirection_count == 0 && fg_info.tdir{i} == 'n'
-                continue
-            end
-
-            if opt.gt_only_roi_count && roi(x,y)==0
-                continue
-            end
-            if cur_mask(x, y)==0
-                continue
-            end
-
-            gtc = gtc + 1;
-        end
-
-        feat = [tmp_dir, gtc, feat];
-        blobFeatures = [blobFeatures; feat];
+        blobs_dir = [blobs_dir; tmp_dir];
     end
+    
+    % get ground truth of each blob
+    blobs_gt = zeros(length(blobs_mask), 1);
+
+    for i=1:size(fg_info.loc, 1)
+        y = round(fg_info.loc(i,1));
+        x = round(fg_info.loc(i,2));
+
+        if x <= 0 || x > size_x || y <=0 || y > size_y
+            continue
+        end
+
+        if opt.gt_only_roi_count && roi(x,y)==0
+            continue
+        end
+
+        if opt.gt_nodirection_count == 0 && fg_info.tdir{i} == 'n'
+            continue
+        end
+        
+        blob_dist = zeros(length(blobs_mask), 1);
+        for j=1:length(blobs_mask)
+            [bx, by] = ind2sub([size_x, size_y], find(blobs_mask{j}));
+            blob_dist(j) = min((bx-x).^2 + (by-y).^2);
+        end
+        [r, idx] = min(blob_dist);
+        blobs_gt(idx) = blobs_gt(idx) + 1;
+    end
+    
+    blobs_Features = [blobs_dir, blobs_gt, blobs_Features];
 end
